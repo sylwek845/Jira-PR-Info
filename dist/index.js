1880c07c829a534e5c7341d2387207a8136f2f11
Sylwester <11651955+sylwek845@users.noreply.github.com>
2024-08-02 10:05:03 +0100

    Update missed param

--- dist/index.js
+++ dist/index.js
@@ -13306,7 +13306,7 @@ const util = __nccwpck_require__(5304);
         core.debug(`#######body ::: ${updatedJiraBody}\n\n`);
         let currentBody = context.payload.pull_request.body
         core.debug(`#######currentBody ::: ${currentBody}`);
-        let updatedPRBody = currentBody.replace(/(<!--jira-body-here-start-->)(.*?)(<!--jira-body-here-end-->)/g, "$1New content$3");
+        let updatedPRBody = currentBody.replace(/(<!--jira-body-here-start-->)(.*?)(<!--jira-body-here-end-->)/g, updatedJiraBody);
         core.debug(`#######updatedBody ::: ${updatedPRBody}`);
         let body = `${updatedPRBody}`
 
